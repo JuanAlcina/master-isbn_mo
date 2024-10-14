d8f9f167625c1e2a819f74e17ae232bf72922adb
JuanAlcina <micorreosar@gmail.com>
2024-10-13 20:16:16 -0500

    Changed app port

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def override_method():
 app.register_blueprint(task_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=4200)
